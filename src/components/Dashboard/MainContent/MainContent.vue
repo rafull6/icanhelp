a2850fc3344e2866e5215cda68a0c5926ee4639e
Vlad <vladyslav.udovychenko@bravelab.io>
2018-11-24 17:39:00 +0100

    Demo multiple markers on map

--- src/components/Dashboard/MainContent/MainContent.vue
+++ src/components/Dashboard/MainContent/MainContent.vue
@@ -12,14 +12,6 @@ export default {
   components: {
     Map
   },
-  mounted() {
-    console.log(google.maps.Map);
-    console.log('chuj')
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-  }
 };
 </script>
 
