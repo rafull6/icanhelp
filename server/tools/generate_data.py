06cc286296561545cd30898ca62ac543e507dd33
bartoszgaj <bartosz.gaj4@gmail.com>
2018-11-24 21:50:22 +0100

    fast fix

--- server/tools/generate_data.py
+++ server/tools/generate_data.py
@@ -53,7 +53,7 @@ def generate_paramedic():
         "rating" : random.randrange(1,5),
         "specialization" : random.choice(["sercownik", "kostnik", "strazak", "policjant"]) ,
         "event_id" : "",
-        "type" :  PARAMEDIC_TYPES[0] if random.random() > 0.9 else PARAMEDIC_TYPES[1]
+        "type" :  PARAMEDIC_TYPES[0] if random.random() > 0.9 else PARAMEDIC_TYPES[1],
         "phone" : str(random.randint(1e9,9e9))
 
     }
