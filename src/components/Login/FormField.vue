6222c9865bd5cc8d3a00bfb9ec8627219b588ae4
Bartosz Wilk <bartek94@gmail.com>
2018-11-24 16:25:44 +0100

    Style login page

---
+++ src/components/Login/FormField.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form-field">
+    <label class="form-field__label">{{ label }}</label>
+    <input class="form-field__input" type="text" :placeholder="placeholder"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormField",
+  props: {
+    label: String,
+    type: String,
+    placeholder: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .form-field {
+    margin-bottom: 25px;
+    display: block;
+    &__label {
+      color: $blue;
+      font-family: 'Exo 2', sans-serif;
+      font-weight: 600;
+      font-size: 15px;
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    &__input {
+      color: $black;
+      background: transparent;
+      font-family: 'Exo 2', sans-serif;
+      font-size: 16px;
+      font-weight: 600;
+      display: block;
+      width: 100%;
+      border: 1px solid $blue;
+      box-sizing: border-box;
+      padding: 15px 20px;
+      &:placeholder-shown {
+        font-weight: 400;
+      }
+    }
+  }
+</style>
