9c5f13c56846030f4ad6ecf1c9bf792beaacbaee
Vlad <vladyslav.udovychenko@bravelab.io>
2018-11-24 16:45:50 +0100

    Map integration

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     Dashboard page
+    <div id="map"></div>
   </div>
 </template>
 
@@ -9,9 +10,34 @@ export default {
   name: "Dashboard",
   props: {
     msg: String
+  },
+  mounted() {
+    console.log(google.maps.Map);
+
+      const map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
   }
+// mounted() {
+  //   console.log('object')
+  //   const api = document.createElement('script');
+  //   api.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBRFc0HsUJAA1JNf71DWH96Gs1Wdz6vb3E";
+  //   api.onload = () => {
+  //     map = new google.maps.Map(document.getElementById('map'), {
+  //       center: {lat: -34.397, lng: 150.644},
+  //       zoom: 8
+  //     });
+  //     console.log(map)
+  //   }
+  // },
 };
 </script>
 
 <style scoped lang="scss">
+#map {
+  background: red;
+  width: 500px;
+  height: 300px;
+}
 </style>
