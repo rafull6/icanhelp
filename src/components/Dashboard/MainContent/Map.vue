dcde35265c50f6b57538f2258c120460f7e80cc4
Vlad <vladyslav.udovychenko@bravelab.io>
2018-11-24 21:08:21 +0100

    Pulsing animation

--- src/components/Dashboard/MainContent/Map.vue
+++ src/components/Dashboard/MainContent/Map.vue
@@ -30,63 +30,65 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-
+<style lang="scss">
 @-moz-keyframes pulsate {
-		from {
-			-moz-transform: scale(0.25);
-			opacity: 1.0;
-		}
-		95% {
-			-moz-transform: scale(1.3);
-			opacity: 0;
-		}
-		to {
-			-moz-transform: scale(0.3);
-			opacity: 0;
-		}
-	}
-	@-webkit-keyframes pulsate {
-		from {
-			-webkit-transform: scale(0.25);
-			opacity: 1.0;
-		}
-		95% {
-			-webkit-transform: scale(1.3);
-			opacity: 0;
-		}
-		to {
-			-webkit-transform: scale(0.3);
-			opacity: 0;
-		}
-	}
-
+  from {
+    -moz-transform: scale(0.25);
+    opacity: 1;
+  }
+  95% {
+    -moz-transform: scale(1.3);
+    opacity: 0;
+  }
+  to {
+    -moz-transform: scale(0.3);
+    opacity: 0;
+  }
+}
+@-webkit-keyframes pulsate {
+  from {
+    -webkit-transform: scale(0.25);
+    opacity: 1;
+  }
+  95% {
+    -webkit-transform: scale(1.3);
+    opacity: 0;
+  }
+  to {
+    -webkit-transform: scale(0.3);
+    opacity: 0;
+  }
+}
 
 #map {
   width: 100%;
   height: 100%;
 }
 
-#map div[title="Need help"] {
-		-moz-animation: pulsate 1.5s ease-in-out infinite;
-		-webkit-animation: pulsate 1.5s ease-in-out infinite;
-		border:1pt solid #fff;
-		-moz-border-radius:51px;
-		-webkit-border-radius:51px;
-		border-radius:51px;
-		-moz-box-shadow:inset 0 0 5px #06f, inset 0 0 5px #06f, inset 0 0 5px #06f, 0 0 5px #06f, 0 0 5px #06f, 0 0 5px #06f;
-		-webkit-box-shadow:inset 0 0 5px #06f, inset 0 0 5px #06f, inset 0 0 5px #06f, 0 0 5px #06f, 0 0 5px #06f, 0 0 5px #06f;
-		box-shadow:inset 0 0 5px #06f, inset 0 0 5px #06f, inset 0 0 5px #06f, 0 0 5px #06f, 0 0 5px #06f, 0 0 5px #06f;
-		height:51px!important;
-		margin:-18px 0 0 -18px;
-		width:51px!important;
-	}
-	#map div[title="Need help"] img {
-		display:none;
-	}
-	@media only screen and (-webkit-min-device-pixel-ratio: 1.5), only screen and (device-width: 768px) {
-		#map div.gmnoprint[title="Need help"] {
-			margin:-10px 0 0 -10px;
-		}
-	}
+#map div[title="active"] {
+  -moz-animation: pulsate 1.5s ease-in-out infinite;
+  -webkit-animation: pulsate 1.5s ease-in-out infinite;
+  border: 1pt solid #fff;
+  -moz-border-radius: 51px;
+  -webkit-border-radius: 51px;
+  border-radius: 51px;
+  -moz-box-shadow: inset 0 0 5px $red, inset 0 0 5px $red, inset 0 0 5px $red,
+    0 0 5px $red, 0 0 5px $red, 0 0 5px $red;
+  -webkit-box-shadow: inset 0 0 5px $red, inset 0 0 5px $red, inset 0 0 5px $red,
+    0 0 5px $red, 0 0 5px $red, 0 0 5px $red;
+  box-shadow: inset 0 0 5px $red, inset 0 0 5px $red, inset 0 0 5px $red,
+    0 0 5px $red, 0 0 5px $red, 0 0 5px $red;
+  height: 51px !important;
+  margin: -15px 0 0 -15px;
+  width: 51px !important;
+}
+#map div[title="active"] img {
+  display: none;
+}
+@media only screen and (-webkit-min-device-pixel-ratio: 1.5),
+  only screen and (device-width: 768px) {
+  #map div.gmnoprint[title="active"] {
+    margin: -10px 0 0 -10px;
+  }
+}
 </style>
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,47 +1,58 @@
 export const generatedData = (google) => {
   return [{
       position: new google.maps.LatLng(-33.91721, 151.2263),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.91539, 151.2282),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.91747, 151.22912),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.9191, 151.22907),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.91725, 151.23011),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.91872, 151.23089),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.91784, 151.23094),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.91682, 151.23149),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.9179, 151.23463),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.91666, 151.23468),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(-33.916988, 151.23364),
-      type: "default"
+      type: "default",
+      title: "active"
     },
     {
       position: new google.maps.LatLng(
@@ -89,7 +100,6 @@ export const generatedData = (google) => {
         151.23348314155578
       ),
       type: "volunteer",
-      title: "Need help"
     }
   ]
 }
