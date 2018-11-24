9e0b1c9919aaa3babb342830ce0284b5f2b12ff3
Vlad <vladyslav.udovychenko@bravelab.io>
2018-11-24 14:12:08 +0100

    Router initial implementation

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    Login page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
